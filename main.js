a12c5c479a2066df7c63bee55d83b8042511cbb6
leilapanova <144453728+leilapanova@users.noreply.github.com>
2024-01-23 13:51:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,167 @@
+// 1
+// let age = prompt('как тебя зовут?')
+// alert(`${age}`)
+
+
+// 2
+// let a = 1, b = 1;
+// let c = ++a;  //2
+// let d = b++;  //2
+
+
+// 3
+// let a = 2;
+// let x = 1 + (a *=2)  //5
+// console.log(x)
+
+
+// 4
+// let a = prompt('Первое число?', 1);
+// let b = prompt('Второе число?', 2);
+
+// alert(+a + +b);
+
+// Переменные:
+
+
+// 1
+// let num = 3
+// alert(num)
+
+
+// 2
+// let a = 10
+//     b = 2
+// alert(a + b)
+// alert(a - b)
+// alert(a * b)
+// alert(a / b)
+
+
+// 3 
+// let c = 15
+//     d = 2
+// let result = c + d
+// alert(result)
+
+
+// 4
+// let a = 10
+//     b = 2
+//     c = 5
+// alert(a + b + c)
+
+
+// 5
+// let a = 17
+//     b = 10
+// let c = a - b
+// let d = 7
+// let result = c + d
+// alert(result)
+
+
+// работа со строками:
+
+// 1
+// let str = 'Привет, мир!'
+// alert(str)
+
+
+// 2
+// let str1 = 'Привет '
+// let str2 = 'мир!'
+// alert(str1 + str2)
+
+
+// 3
+// let name = 'Leila'
+// alert(`Привет ${name}`)
+
+
+// 4
+// let age = 15
+// alert(`Мне ${age} лет`)
+
+
+// prompt1
+// let name = prompt('Как тебя зовут?');
+// alert(`Ваше имя ${name}`)
+
+
+// 2
+// let num = prompt('Назовите число')
+// alert(num + num)
+
+
+// Обращение к символам строки:
+
+// 1
+// let str = 'abcde'
+// alert(str[0])
+// alert(str[2])
+// alert(str[4])
+
+
+// 2
+// let num = '12345'
+// alert(num[0] * num[1] * num[2] * num[3] * num[4])
+
+
+// Практика:
+
+// 1
+// let secondinhour = 60 * 60 
+// alert(`Секунд в часе: ${secondinhour}`) 
+ 
+// let secondsinday = secondinhour * 24 
+// alert(`Секунд в дне: ${secondsinday}`) 
+ 
+// let secondinmonth = secondsinday * 30 
+// alert(`Секунд в месяце: ${secondinmonth}`) 
+ 
+
+//2 
+// let now = new Date() 
+//     sec = now.getSeconds() 
+//     min = now.getMinutes() 
+//     hour = now.getHours() 
+// alert(hour + ":" + min + ":" + sec) 
+ 
+
+//3 
+// let num = 23 
+// alert(num * num) 
+ 
+
+//4 
+// var num = 47 
+// num += 7 
+// num -= 18 
+// num *= 10 
+// num /= 15 
+// alert(num) 
+ 
+
+//5 
+// var num = 10; 
+// num++; 
+// num++; 
+// num--; 
+// alert(num)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
